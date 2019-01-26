0d106a2fa6ac4f3ed1b670ec94f398d27b579db6
cjl <605385881@qq.com>
2019-01-26 17:21:46 +0800

    old spring,struts2 ok

--- src/com/cjl/action/LoginAction.java
+++ src/com/cjl/action/LoginAction.java
@@ -1,10 +1,17 @@
 package com.cjl.action;
 
+import com.cjl.service.LoginService;
 import com.opensymphony.xwork2.ActionSupport;
 
 public class LoginAction extends ActionSupport {
     private String name;
     private String password;
+
+    private LoginService service;
+
+    public void setService(LoginService service) {
+        this.service = service;
+    }
 
     public String getName() {
         return name;
@@ -24,6 +31,9 @@ public class LoginAction extends ActionSupport {
 
     @Override
     public String execute() throws Exception {
-        return super.execute();
+        if (service.isLogin(name,password)) {
+            return SUCCESS;
+        }
+        return ERROR;
     }
 }
